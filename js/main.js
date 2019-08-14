a33afc13a0fafc6dd2d3c109a245cef7b0364cc2
fzg8189 <aakash.ohri@varian.com>
2019-08-14 17:05:32 +0530

    added typed.js library

--- js/main.js
+++ js/main.js
@@ -66,4 +66,38 @@ $(document).ready(function() {
 
     scrollTo($.attr(this, "href"));
   });
+
+  var typed = new Typed('.highlight', {
+  strings: ["Aakash Ohri.", "a Developer.", " a Geek.", "an Indian."],
+    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+    stringsElement: null,
+    // typing speed
+    typeSpeed: 90,
+    // time before typing starts
+    startDelay: 2000,
+    // backspacing speed
+    backSpeed: 30,
+    // time before backspacing
+    backDelay: 1500,
+    // loop
+    loop: true,
+    // false = infinite
+    loopCount: 500,
+    // show cursor
+    showCursor: true,
+    // character for cursor
+    cursorChar: "|",
+    // attribute to type (null == text)
+    attr: null,
+    // either html or text
+    contentType: 'html',
+    // call when done callback function
+    callback: function() {},
+    // starting callback function before each string
+    preStringTyped: function() {},
+    //callback for every typed string
+    onStringTyped: function() {},
+    // callback for reset
+    resetCallback: function() {}
+    });
 });
